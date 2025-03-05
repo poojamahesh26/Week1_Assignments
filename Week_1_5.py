f5e4c2cb6a043a2b25c0c7a61561efaa19087ceb
poojamahesh26 <pm4005@nyu.edu>
2025-03-04 23:29:51 -0500

    Week_1_5

---
+++ Week_1_5.py
@@ -0,0 +1,5 @@
+# ⁠Iterate through the following list of animals and print each one in all caps.
+
+animals = ['tiger', 'elephant', 'monkey', 'zebra', 'panther'] 
+for animal in animals:
+    print(animal.upper())
